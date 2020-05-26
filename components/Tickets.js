107d0b80da6e27e0038f4d8c76e5520c97df7260
Sarah German <sgerman@pantheon.io>
2020-05-26 11:31:15 -0500

    Add alert icon if ticket has 'escalated' tag.

--- components/Tickets.js
+++ components/Tickets.js
@@ -7,9 +7,16 @@ const Tickets = (props) => {
   let ticketList = [];
 
   for (const [index, ticket] of tickets.entries()) {
+    let icon = "";
+    if (ticket.escalated == "true") {
+      icon = "🚨";
+    }
+
     ticketList.push(
       <li key={index + "-ticket"} className="list-disc ml-4">
+        <span>{icon}</span>
         <a href={`${zendeskBaseURL}/${ticket.id}`}>{ticket.subject}</a>
+        <span className="ml-1 text-sm text-gray-700">[{ticket.status}]</span>
       </li>
     );
   }
