653346d1b2be1d3d90738862e2c8de035ac7508d
Sarah German <sgerman@pantheon.io>
2020-08-18 09:36:14 -0500

    Update startup script

--- scripts/build-dash.sh
+++ scripts/build-dash.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Build the team report CSV
-cseds --team
+cseds --team --dest=./public/data
 
-# Move it over here
-mv ~/projects/cseds-cli/tmp/team-report.csv ~/projects/cseds-dashboard/public/data/
+# Fire up the site
+npm run dev
+
+# Note the time so we can show this in the app
+# @TODO write this to a json file maybe?
